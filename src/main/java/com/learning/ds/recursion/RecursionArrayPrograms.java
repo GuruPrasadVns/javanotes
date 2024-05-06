3fe46e430f09b0c8e7e7cd5716cc1ab540788785
prasag1 <guru.prasad@bsci.com>
2024-05-06 14:36:31 +0530

    reverse array rec

--- src/main/java/com/learning/ds/recursion/RecursionArrayPrograms.java
+++ src/main/java/com/learning/ds/recursion/RecursionArrayPrograms.java
@@ -17,5 +17,22 @@ public class RecursionArrayPrograms {
 		}
 		return -1;
 	}
+	
+	
+	public static int[] reverseArrayRec(int[] arr, int low, int high) {
+		if(low > high)
+			return arr;
+		
+		swap(arr, low , high);
+		
+		return reverseArrayRec(arr, ++low, --high);
+	}
+
+
+	private static void swap(int[] arr, int i, int j) {
+		int temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;			
+	}
 
 }
--- src/test/java/com/learning/ds/recursion/RecursionArrayProgramsTest.java
+++ src/test/java/com/learning/ds/recursion/RecursionArrayProgramsTest.java
@@ -1,5 +1,6 @@
 package com.learning.ds.recursion;
 
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
 
 import org.junit.Test;
@@ -14,4 +15,13 @@ public class RecursionArrayProgramsTest {
 		assertEquals(3, RecursionArrayPrograms.binarySearchRecursive(arr, 0, arr.length - 1, 40));
 		assertEquals(-1, RecursionArrayPrograms.binarySearchRecursive(arr, 0, arr.length - 1, 5));
 	}
+	
+	@Test
+	public void arrayReverseRecTest() {
+		int[] arr = {10, 20, 30, 40, 50, 60, 70, 80, 90};
+		int[] expectedArray = {90,80, 70, 60, 50, 40, 30, 20, 10};
+		
+		assertArrayEquals(expectedArray, RecursionArrayPrograms.reverseArrayRec(arr, 0, arr.length - 1));
+	}
+
 }
